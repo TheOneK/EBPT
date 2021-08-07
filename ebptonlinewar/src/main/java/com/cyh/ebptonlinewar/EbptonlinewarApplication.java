2288cd42519e1478fdfbbae72a451787beaf08f6
cyh <958634974@qq.com>
2021-08-07 16:47:35 +0800

    修改jar包名称

--- ebptonlinewar/src/main/java/com/cyh/ebptonlinewar/EbptonlinewarApplication.java
+++ ebptonlinewar/src/main/java/com/cyh/ebptonlinewar/EbptonlinewarApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EbptonlinewarApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(EbptonlinewarApplication.class, args);
     }
 
