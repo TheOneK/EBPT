8f092f0e66de3f00bbd5b51e85d147d3586667a4
cyh <958634974@qq.com>
2021-08-15 01:03:24 +0800

    添加junit5 单元测试框架

---
+++ ebptonline-war/src/test/java/com/cyh/ebptonlinewar/test/testTest.java
@@ -0,0 +1,66 @@
+package com.cyh.ebptonlinewar.test;
+
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class testTest {
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("**--- Executed once before all test methods in this class ---**");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("**--- Executed before each test method in this class ---**");
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"Effective Java", "Code Complete", "Clean Code"})
+    void testPrintTitle(String title) {
+        System.out.println(title);
+    }
+    @Test
+    void testMethod1() {
+        System.out.println("**--- Test method1 executed ---**");
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,One", "2,Two", "3,Three"})
+    void testDataFromCsv(long id, String name) {
+        System.out.printf("id: %d, name: %s", id, name);
+    }
+
+    @DisplayName("Test method2 with condition")
+    @Test
+    void testMethod2() {
+        System.out.println("**--- Test method2 executed ---**");
+    }
+
+    @Test
+    @Disabled("implementation pending")
+    void testMethod3() {
+        System.out.println("**--- Test method3 executed ---**");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("**--- Executed after each test method in this class ---**");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("**--- Executed once after all test methods in this class ---**");
+    }
+
+
+
+
+}
