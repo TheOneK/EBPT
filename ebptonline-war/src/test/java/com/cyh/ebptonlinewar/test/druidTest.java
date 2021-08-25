6188cdbbd6a4deb1918fbafab50444607bbdcaed
ThOneK <958634974@qq.com>
2021-08-25 14:10:03 +0800

    1.修改所有使用的链接的类 2.删除无效启动类

--- ebptonline-war/src/test/java/com/cyh/ebptonlinewar/test/druidTest.java
+++ ebptonline-war/src/test/java/com/cyh/ebptonlinewar/test/druidTest.java
@@ -1,28 +1,24 @@
 package com.cyh.ebptonlinewar.test;
 
 
-import com.cyh.ebptonlinewar.EbptonlineWarApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
-@SpringBootTest(classes = EbptonlineWarApplication.class)
-@WebAppConfiguration
+@SpringBootTest
 public class druidTest {
     @Autowired
     private DataSource dataSource;
 
     @Test
-    private void testDruid() throws SQLException {
+    public void testDruid() throws SQLException {
         System.out.println(dataSource.getClass());
         Connection connection = dataSource.getConnection();
         System.out.println(connection);
         connection.close();
-
     }
 
 }
--- ebptonline-war/src/test/java/com/cyh/ebptonlinewar/test/junitTest.java
+++ ebptonline-war/src/test/java/com/cyh/ebptonlinewar/test/junitTest.java
@@ -11,7 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class junitTest {
     @BeforeAll
     static void beforeAll() {
