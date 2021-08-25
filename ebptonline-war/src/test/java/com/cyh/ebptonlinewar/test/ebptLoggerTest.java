a192d8de9fcd7a6799018e235e35d1dac7eee5ff
ThOneK <958634974@qq.com>
2021-08-25 10:26:15 +0800

    修改import类，将org.junit.test改成org.junit.jupiter.api.Test

--- ebptonline-war/src/test/java/com/cyh/ebptonlinewar/test/ebptLoggerTest.java
+++ ebptonline-war/src/test/java/com/cyh/ebptonlinewar/test/ebptLoggerTest.java
@@ -1,17 +1,16 @@
 package com.cyh.ebptonlinewar.test;
 
 
-import com.cyh.ebptonlinewar.EbptonlineWarApplication;
 import com.cyh.ebptpubjar.common.Constant;
 import com.cyh.ebptpubjar.ebptLogger.EbptLogger;
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.HashMap;
 import java.util.Map;
 // 获取启动类，加载配置，确定装载 Spring 程序的装载方法，它回去寻找 主配置启动类（被 @SpringBootApplication 注解的）
-@SpringBootTest(classes = {EbptonlineWarApplication.class})
+@SpringBootTest
 public class ebptLoggerTest {
 
     @Test
