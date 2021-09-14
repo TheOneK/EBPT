729395631e38428376096db1445ddf5474555755
ThOneK <958634974@qq.com>
2021-09-14 19:56:51 +0800

    优化代码

--- ebpt-admin/src/main.js
+++ ebpt-admin/src/main.js
@@ -1,10 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Element from 'element-ui'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Cookies from 'js-cookie'
 
 Vue.config.productionTip = false
+
+Vue.use(Element, {
+  size: Cookies.get('size') || 'medium' // set element-ui default size
+})
 
 /* eslint-disable no-new */
 new Vue({
